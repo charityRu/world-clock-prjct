46b13057f5137296be5da79c7564355cfcc274bc
charity <nicolecharity22@gmail.com>
2024-12-03 14:22:57 -0800

    Added timezone functionality and improved select behavior

--- javascript/index.js
+++ javascript/index.js
@@ -18,3 +18,27 @@ if (sydneyElement) {
     sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
     sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss[<small>] A[</small>]");
 }
+
+function updateCity(event){
+    let cityTimeZone=event.target.value;
+    let cityTime=moment().tz(cityTimeZone);
+    let citiesElement=document.querySelector("#cities");
+    citiesElement.innerHTML= 
+    `
+    <div class="city" >
+       <div>
+        <h2>${cityTimeZone}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}
+        </div>
+    </div>
+        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
+    </div>`
+}
+
+
+
+
+
+
+let citiesSelectElement=document.querySelector("#city")
+citiesSelectElement.addEventListener("change",updateCity)
