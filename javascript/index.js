f50ea22eb50f76e5155bf4c48661ad855ce4f610
charity <nicolecharity22@gmail.com>
2024-12-04 12:56:37 -0800

    updated current location functionality

--- javascript/index.js
+++ javascript/index.js
@@ -21,6 +21,9 @@ if (sydneyElement) {
 
 function updateCity(event){
     let cityTimeZone=event.target.value;
+    if(cityTimeZone==="current"){
+        cityTimeZone=moment.tz.guess();
+    }
     let cityTime=moment().tz(cityTimeZone);
     let citiesElement=document.querySelector("#cities");
     citiesElement.innerHTML= 
